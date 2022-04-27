dfa8524f11a35219ab2179135fdcd67c9bff1bdb
lngu0070 <lngu0070@student.monash.edu>
2022-04-27 17:43:21 +1000

    UI - log in screen - sign up screen
    NSObject
    - Account

---
+++ whatsnext/Account.swift
@@ -0,0 +1,17 @@
+//
+//  Account.swift
+//  whatsnext
+//
+//  Created by Nguyen Linh Chi on 27/4/2022.
+//
+
+import UIKit
+
+class Account: NSObject {
+    
+    var username: String! // key
+    var password: String!
+    var firstname: String!
+    var lastname: String!
+    
+}
---
+++ whatsnext/LogInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LogInViewController.swift
+//  whatsnext
+//
+//  Created by Nguyen Linh Chi on 27/4/2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    // UI ELEMENTS !!!!!!!!!!!!!!!
+    
+    @IBOutlet weak var appLogo: UIView!
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func loginBT(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
