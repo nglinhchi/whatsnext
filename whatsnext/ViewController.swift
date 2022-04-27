cf18ddb61189ef4a721089ffc908ab1acb285d38
lngu0070 <lngu0070@student.monash.edu>
2022-04-27 16:16:48 +1000

    Initial Commit

---
+++ whatsnext/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatsnext
+//
+//  Created by Nguyen Linh Chi on 27/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
